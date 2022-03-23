287046e18e6d3834f272b87a8a205b662014b327
RUDONI Antonin <antonin.rudoni3@etu.univ-lorraine.fr>
2022-03-23 17:32:23 +0100

    html

--- projet-photobox-squelette/photobox.js
+++ projet-photobox-squelette/photobox.js
@@ -1,8 +1,7 @@
 import gallery from "./gallery.js"
 import gallery_ui from "./gallery_ui.js"
 
-document.querySelector('#load_gallery')
-  .addEventListener('click',  e => {
+document.querySelector('#load_gallery').addEventListener('click',  e => {
     gallery.load().then(gallery_ui.display_gallery)
   })
 
